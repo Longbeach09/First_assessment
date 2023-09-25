2717f4dc44bbd190fcd47a5334e16e7ffa6cc299
gabriel ward <mokeward47@gmail.com>
2023-09-24 22:58:50 -0600

    the rest of the questions

--- src/arrays.js
+++ src/arrays.js
@@ -3,43 +3,80 @@
 //   greaterThanTen([1, 2, 3, 11, 12, 13]);
 //   => [11, 12, 13]
 function greaterThanTen(numbers) {
-  let bigNum = []
+  let bigNum = [];
 
-  for(let value of numbers){
-    bigNum.push(value < 10 )}
-    
-  return bigNum
+  for (let number of numbers) {
+    //aying let the numbers in the numbers array
+    if (number > 10) bigNum.push(number); //continuing line 8  its saying if the number us bigger 10 push the number into a
+    //vairiable called bigNum
+  }
+
+  return bigNum;
 }
 
+// console.log(greaterThanTen([1, 2, 3, 11, 12, 13]))
 
 // Given an array of strings, return all words that start with 'b' or 'B'.
 // Ex.:
 //   bWords(['banana', 'orange', 'apple', 'Bonobo', 'kiwi', 'pear']);
 //   => ['banana', 'Bonobo]
 function bWords(words) {
-let wordsWithB= []
-for ()
+  let wordsWithB = []; //defining a array calling it workdsWithB
 
+  for (let word of words) {
+    //same as the last one its laying let the words in the words array
+    if (word.toLowerCase().startsWith("b")) wordsWithB.push(word); //saying lowerCase the word and if it starts with b push it into the array
+  }
+
+  return wordsWithB;
 }
+// console.log(bWords(['banana','orange','apple','Bonobo']))
 
 // Add all the elements from additionalItems to the end of originalArray.
 // Return the originalArray..
 // Ex.:
 //  extend([1, 2, 3], [4, 5, 6]);
 //  => [1, 2, 3, 4, 5, 6]
-function extend(originalArray, additionalItems) {}
+function extend(originalArray, additionalItems) {
+  // Utilizing "spread" syntax
+  return [...originalArray, ...additionalItems]; //this i googled and it kinda made sense but hopfully we can go over it
+
+  // return originalArray.concat(additionalItems);
+
+  // for(let i = 0; i < additionalItems.length; i++){
+  //   originalArray.push(additionalItems[i])
+  // }
+
+  // additionalItems.forEach(item => originalArray.push(item))
+
+  // return originalArray;
+}
 
 // Return an array of all items with the given length.
 // Ex.:
 //   itemsWithLength(['a', 'bbb', 'cccc', 'dddddd', 'eee'], 3);
 //   => ['bbb', 'eee']
-function itemsWithLength(items, length) {}
+function itemsWithLength(items, length) {
+  const temp = []; //same as very im naming a array called temp
+  for (let i = 0; i < items.length; i += 1) {
+    //this is going thru the look untill it reaches the end
+    if (items[i].length === length) temp.push(items[i]); //this says if the items[i's]length is = to the length push it to the array
+  }
+  return temp; //returning the array
+}
 
 // Return an array with every other element from the input array (start with index 0).
 // Ex.:
 //   everyOtherItem(['a', 'b', 'c', 'd', 'e']);
 //   => ['a', 'c', 'e']
-function everyOtherItem(items) {}
+function everyOtherItem(items) {
+  const temp = []; // setting a array and naming it temp
+  for (let i = 0; i < items.length; i += 1) {
+    //same thing going trough untill it reaches the end
+    if (i % 2 === 0) temp.push(items[i]); //this is saying every other letter push into a array(had to google the "i % 2")
+  }
+  return temp; // return the
+}
 
 // Given a list of words and a letter, return the indexes of the words that
 // start with that letter. You can assume that the words and letter will always
@@ -47,7 +84,14 @@ function everyOtherItem(items) {}
 // Ex.:
 //   findWordsStartingWith(['apple', 'banana', 'kiwi', 'pear', 'bacon'], 'b');
 //   => [1, 4]
-function findWordsStartingWith(words, letter) {}
+function findWordsStartingWith(words, letter) {
+  const temp = []; // naming a array temp
+  for (let i = 0; i < words.length; i += 1) {
+    //same thing going thru the whole function
+    if (words[i].startsWith(letter)) temp.push(i); // sayinf if the index of words starts with the letter push it to the array
+  }
+  return temp; // returning the array
+}
 
 // Return the `n` smallest values in the array in descending order (largest
 // numbers first). Assume that `n` will always be less than the length of the
@@ -55,7 +99,24 @@ function findWordsStartingWith(words, letter) {}
 // Ex.:
 //   smallestNItems([1, 30, 4, 21, 100000], 3);
 //   => [21, 4, 1]
-function smallestNItems(items, n) {}
+function smallestNItems(items, n) {
+  // Clone the items array to a new array
+  const sortedItems = [...items];
+
+  // Sort the array in descending order
+  // for (let i = 0; i < sortedItems.length; i++) {
+  //   for (let j = i + 1; j < sortedItems.length; j++) {
+  //     if (sortedItems[i] < sortedItems[j]) {
+  //       const temp = sortedItems[i];
+  //       sortedItems[i] = sortedItems[j];
+  //       sortedItems[j] = temp;
+  //     }
+  //   }
+  //  }
+
+  //   // Return n number of smallest items from the sorted array
+  //   return sortedItems.slice(sort edItems.length - n);
+}
 
 // Search for a value in the array and return its index. If the value appears
 // more than once, return the index of the *FIRST* occurrence of the value. If
@@ -63,14 +124,26 @@ function smallestNItems(items, n) {}
 // Ex.:
 //   findIndex(['a', 'b', 'c', 'a', 'b', 'c'], 'c');
 //   => 2
-function findIndex(items, value) {}
+function findIndex(items, value) {
+  for (let i = 0; i < items.length; i += 1) {
+    // going thru the whole thing like every other one
+    if (items[i] === value) return i; // sayinf of the items index is equal to the value return index
+  }
+}
 
 // Given a start number and stop number, return a new array containing all the numbers
 // between the start and stop number.
 // Ex.:
 //   range(1, 5);
 //   => [1, 2, 3, 4, 5]
-function range(start, stop) {}
+function range(start, stop) {
+  const temp = []; //setting a array called temp
+  for (let i = start; i <= stop; i += 1) {
+    // sayin the loop starts at the star and stops at the end preety simple
+    temp.push(i); //pushes it to the array
+  }
+  return temp;
+}
 
 export {
   bWords,
--- src/functions.js
+++ src/functions.js
@@ -3,7 +3,7 @@
 //   divide(10, 2);
 //   => 5
 function divide(x, y) {
-  return x / y
+  return x / y; // returns x divided by y
 }
 
 // Return the average of x and y.
@@ -13,7 +13,7 @@ function divide(x, y) {
 //   average(10, 2);
 //   => 6
 function average(x, y) {
-  return (x + y) / 2
+  return (x + y) / 2; // returns the average
 }
 
 // Return true if x and y are mostly equivalent up to a tolerance of 0.001.
@@ -22,9 +22,7 @@ function average(x, y) {
 //   approximatelyEqual(10.001, 10);
 //   => true
 function approximatelyEqual(x, y) {
-  if(x - y <.001)
-  return  true
-
+  if (x - y < 0.001) return true; //saying if x is close enough return true
 }
 
 // Given a first name and last name, return a full name in the format "FIRST LAST"
@@ -32,10 +30,11 @@ function approximatelyEqual(x, y) {
 //   fullName('John', 'Doe');
 //   => 'John Doe'
 function fullName(firstName, lastName) {
-  return `${firstName}${lastName}`
-
-  
-} 
+  //
+  // return firstName + " " + lastName;
+  // return [firstName, lastName].join(" ");
+  return `${firstName} ${lastName}`; //printing last and first name togeth
+}
 
 // Generate the sentence "PERSON was drinking BEVERAGE at LOCATION" using the
 // person, beverage and location provided.
@@ -43,8 +42,7 @@ function fullName(firstName, lastName) {
 //   generateSentence('Kay', 'coffee', 'the local cafe');
 //   => 'Kay was drinking coffee at the local cafe.'
 function generateSentence(person, beverage, location) {
- return `${person} was drinking${beverage} at${location}.`
-
+  return `${person} was drinking ${beverage} at ${location}.`; //same as the last printing the first and last one
 }
 
 // Return the given string with all vowels replced with '*' characters.
@@ -52,15 +50,33 @@ function generateSentence(person, beverage, location) {
 //   censorVowels('javascript');
 //   => 'j*v*scr*pt'
 function censorVowels(string) {
+  // create an array of censorVowels
+  // Loop over the string and if the character matches a vowel, then replace it with a *
+  const split = string.split("");
+  const vowels = ["a", "e", "i", "o", "u"]; // setting an array of vowels
+  for (let i = 0; i < split.length; i += 1) {
+    //going thru the whole function
+    if (vowels.includes(split[i])) {
+      //saying if vowels is incluted split index
+      split[i] = "*"; //  saying of the split things are equal to *
+    }
+  }
+  return split.join(""); //joined them together
 
-
+  //used a weird google answer for this one im sure i could have optimized it but i couldnt fiqure it out
 }
 
 // Return the given string in sticky case.
 // Ex.:
 //   stickyCase('hello world');
 //   => 'hElLo wOrLd'
-function stickyCase(string) {}
+function stickyCase(string) {
+  // const split = string.split("");
+  // for (let i = 0; i < split.length; i += 1) {
+  //   if (i % 2 === 1) split[i] = split[i].toUpperCase();
+  // }
+  // return split.join("");
+}
 
 // Return the given string in leetspeak. Leetspeak is a modified version of
 // English where characters are replaced by numbers or symbols. For this
@@ -74,7 +90,27 @@ function stickyCase(string) {}
 // Ex.:
 //   leetspeak('javascript');
 //   => 'j4v45cr1p7'
-function leetspeak(string) {}
+const leetSpeakMap = {
+  a: "4",
+  e: "3",
+  i: "1",
+  o: "0",
+  s: "5",
+  t: "7",
+};
+
+function leetspeak(string) {
+  // const split = string.split(""); // split the string input into an array
+  // const charactersToSwap = Object.keys(leetSpeakMap); // get the keys from `leetSpeakMap` as an array, IE: ['a', 'e', 'i', 'o', 's', 't']
+  // for (let i = 0; i < split.length; i += 1) {
+  //   // loop over split string array
+  //   if (charactersToSwap.includes(split[i])) {
+  //     // check if the current character in the split string array is included in the leetSpeakMap keys array (charactersToSwap)
+  //     split[i] = leetSpeakMap[split[i]]; // If theres a match, then assign that character to its equivilant mapped "leet speak" character from `leetSpeakMap`
+  //   }
+  // }
+  // return split.join(""); // Return the array as a string after joining it back into string form
+}
 
 export {
   approximatelyEqual,
